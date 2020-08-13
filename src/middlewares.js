8565a0704a0bfdf8b8a8a81db47b7e3f4e844e9e
Pedro Paulo dos Santos <dr2p@hotmail.com>
2020-08-13 10:21:46 -0300

    Update middlewares.js

--- src/middlewares.js
+++ src/middlewares.js
@@ -32,7 +32,12 @@ function guard(req, res, next) {
 
   jwt.verify(token, authConfig.secret, (err, decoded) => {
     if (err) return res.status(401).send({ error: 'Token invalid' })
-    res.user_id = decoded.id
+    res.user_id = decoded.id  
+    
+    /* Esse é o user_id que fica aparecendo na saída da api manifest. Ele recebe o token pelo header da requisição 
+    e descriptografa para extrair o payload inserido nele. Como nesse commit o payload do signup/signin é só o id
+    retornaremos na response o user_id. Caso sejam necessários mais dados como email, senha, username ou grupo, esses
+    devem ser inseridos no payload na seção de signin/signup.*/
 
     return next()
   })
