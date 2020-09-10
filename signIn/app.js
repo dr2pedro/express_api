2f6ccf0b7be210db71446a9bf377720da4f6478f
Pedro Paulo dos Santos <dr2p@hotmail.com>
2020-09-10 16:40:53 -0300

    Update app.js

--- signIn/app.js
+++ signIn/app.js
@@ -1,17 +1,20 @@
 const express = require('express')
 const morgan = require('morgan')
 const helmet = require('helmet')
+const cors = require('cors')
 
 require('dotenv').config()
 
 const signIn = require('./src/routes')
+const whitelist = ["http://localhost:2000"]
 
 const app = express()
 
 app.use(morgan('dev'))
 app.use(helmet())
 app.use(express.json())
-
+app.use(cors({origin:whitelist})
+        
 app.use('/', signIn)
 
 module.exports = app
